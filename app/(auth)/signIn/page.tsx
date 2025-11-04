a39fade2e47a056813d941b784848ac8afe75b8a
Kent0710 <https://github.com/Eduard-K-A/temp-project-launchpad>
2025-11-04 19:55:05 +0800

    refactor: use the new loader component for the button in sign in page

--- app/(auth)/signIn/page.tsx
+++ app/(auth)/signIn/page.tsx
@@ -2,16 +2,21 @@
 
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "@/lib/firebase/firebase";
-import { Button } from "@/components/ui/button";
 import { FaGoogle } from "react-icons/fa";
 
 import { setSession } from "@/actions/auth";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
+import LoaderButton from "@/components/reusables/loader-button";
 
 export default function SignUpPage() {
+    const [loading, setLoading] = useState(false);
     const router = useRouter();
 
     const handleGoogleSignIn = async () => {
+        setLoading(true);
+
         const provider = new GoogleAuthProvider();
         const result = await signInWithPopup(auth, provider);
         const idToken = await result.user.getIdToken();
@@ -19,15 +24,22 @@ export default function SignUpPage() {
         const sessionResult = await setSession(idToken);
         if (sessionResult.status === "success") {
             router.push("/home");
+        } else {
+            setLoading(false);
+            toast.error("Failed to sign in. Please try again.");
         }
     };
 
     return (
         <div className="flex flex-col items-center justify-center h-dvh">
-            <Button onClick={handleGoogleSignIn}>
+            <LoaderButton
+                loadingText="Signing in..."
+                isLoading={loading}
+                onClick={handleGoogleSignIn}
+            >
                 <FaGoogle />
                 Sign in with Google
-            </Button>
+            </LoaderButton>
         </div>
     );
 }
